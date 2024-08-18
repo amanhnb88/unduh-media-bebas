cb6729c24994a3756575aaf49d7ba56913217c5b
KwiatekMiki <79092746+KwiatekMiki@users.noreply.github.com>
2024-08-18 10:34:00 +0200

    app: add caching

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, send_from_directory
 from json import load
 from cache import cache_for
+from flask_caching import Cache
 
+dev = True
 commit = "COMMIT"
 app = Flask(__name__)
+app.config["CACHE_TYPE"] = "SimpleCache"
+cache = Cache(app)
 
 class Colors:
     green = ""
@@ -15,6 +19,7 @@ def getinstances():
     return load(open('output/instances.json'))
 
 @app.route('/')
+@cache.cached(timeout=60 if not dev else 1)
 def index():
     instances = getinstances()
     return render_template("index.html",
@@ -22,9 +27,11 @@ def index():
     )
 
 @app.route('/api')
+@cache.cached(timeout=120)
 def api():
     return render_template("api.html", domain=request.host_url.rstrip("/"))
 
 @app.route('/api/instances.json')
+@cache.cached(timeout=60 if not dev else 1)
 def api_instances():
     return send_from_directory("output", "instances.json")
