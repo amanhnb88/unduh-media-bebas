83f952e721244e05ae20d04779067da5fffdaea7
thelocaltemp <thelocaltemp@noreply.codeberg.org>
2024-10-13 17:36:05 +0200

    feat: added searching

--- static/options.js
+++ static/options.js
@@ -2,41 +2,75 @@ classes = {
     "safe": ".trust0, .trust-1",
     "online": ".score-1",
     "frontend": ".frontendFalse",
-    "domainonly": ".nodomainTrue"
+    "domainonly": ".nodomainTrue",
+    "search": ".search"
 }
 
-function createStyle(option) {
-    const style = document.createElement("style");
-    style.id = option + "style";
-    document.querySelector("#optionstuff").appendChild(style);
-    return document.querySelector(`#${option}style`);
-}
+instances = document.querySelectorAll(`*[data-is-instance]`)
+checkboxes = document.querySelectorAll('#settings > * input[type="checkbox"]')
+search = document.querySelector('#search')
 
-function handleOption(option, checked, classestohide) {
+checkboxes.forEach((input) => {
+    handleOption(input.id, input.checked, classes[input.id])
+
+    input.addEventListener("change", function() {
+        handleOption(input.id, input.checked, classes[input.id])
+    })
+})
+
+handleSearch(search.id, search.value)
+
+search.addEventListener("input", function() {
+    handleSearch(search.id, search.value)
+})
+
+// Styling functions
+
+function handleOption(option, active, classestohide) {
+    console.log("3. handleoption - option: " + option + " - active: " + active + " - classestohide: " + classestohide)
+
     let optionstyle;
     optionstyle = document.querySelector(`#${option}style`);
+
     if (!optionstyle) {
         optionstyle = createStyle(option);
     }
 
-    if (checked) {
+    if (active) {
         optionstyle.innerText = `${classestohide} { display: none; }`;
     } else {
         optionstyle.innerText = "";
     }
 }
 
-function updateOption(option) {
-    const checkbox = document.getElementById(option);
-    handleOption(option, checkbox.checked, classes[option])
+function handleSearch(option, active) {
+    console.log("3. handleoption_search - option: " + option + " - active: " + active)
+
+    if (active) {
+        instances.forEach((instance) => {
+            if (instance.dataset.api.includes(active) || instance.dataset.frontend.includes(active)) {
+                console.log(instance)
+                instance.classList.add("show")
+                instance.classList.remove("hide")
+            } else {
+                instance.classList.add("hide")
+                instance.classList.remove("show")
+
+            }
+        })
+    } else {
+        instances.forEach((instance) => {
+            instance.classList.add("show")
+            instance.classList.remove("hide")
+        })
+    }
 }
 
-document.querySelectorAll("#settings > * label").forEach((label) => {
-    const setting = label.getAttribute("for");
-    const checkbox = document.getElementById(setting);
+function createStyle(option) {
+    console.log("4. createstyle - option: " + option)
 
-    updateOption(setting);
-    checkbox.addEventListener("change", function() {
-        updateOption(this.id)
-    })
-})
+    const style = document.createElement("style");
+    style.id = option + "style";
+    document.querySelector("#optionstuff").appendChild(style);
+    return document.querySelector(`#${option}style`);
+}
