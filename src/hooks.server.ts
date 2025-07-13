a5a720cd2799d9267b8a2d1588e39fa13c37dcec
KwiatekMiki <hi@kwiatekmiki.pl>
2025-07-13 02:08:08 +0200

    feat: add support for scanning on start
    automatically enabled, set `SCAN_ON_START` to `false` to disable.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,10 +2,14 @@ import { getInstances } from "$lib/instances";
 import { spawn } from "child_process";
 declare const env: Record<string, string | undefined>;
 const scanInterval: string | undefined = env.SCAN_INTERVAL;
+const scanOnStart: string | undefined = env.SCAN_ON_START;
 const scan = () =>
     spawn("node", ["src/lib/scan.ts"], { stdio: "inherit" });
 
-if ((await getInstances()).length < 1) scan();
+if ((await getInstances()).length < 1 || scanOnStart !== "false") {
+    scan();
+}
+
 if (scanInterval && +scanInterval > 10) {
     setInterval(scan, +scanInterval * 60000);
 }
