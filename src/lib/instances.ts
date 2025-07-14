3da2895805b176509ca69d79dc6d1ddfd61595cc
KwiatekMiki <hi@kwiatekmiki.pl>
2025-07-14 10:00:00 +0200

    feat: prevent potential api key leakages

--- src/lib/instances.ts
+++ src/lib/instances.ts
@@ -5,7 +5,8 @@ export async function getInstances(): Promise<Instance[]> {
     try {
         const file: Buffer = await readFile("src/lib/output/instances.json");
         return JSON.parse(file.toString())
-            .sort((a: any, b: any) => (b.score || 0) - (a.score || 0));
+            .sort((a: any, b: any) => (b.score || 0) - (a.score || 0))
+            .filter(a => a.toString().toLowerCase().includes("api-key"));
     } catch {
         return [];
     }
